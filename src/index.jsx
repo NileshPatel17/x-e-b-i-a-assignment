1c356d06959d4425a67e72f2706b3f9220024f5b
Nilesh Patel <nilesh.nvs@hotmail.com>
2019-12-06 22:22:52 +0530

    added errror-boundary

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from './providers';
 import './styles/tailwind.css';
 import App from './App';
+import { ErrorBoundary } from './components/error-boundary';
 
 function Main() {
   return (
-    <Provider>
-      <App />
-    </Provider>
+    <ErrorBoundary>
+      <Provider>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   );
 }
 ReactDOM.render(<Main />, document.getElementById('root'));
